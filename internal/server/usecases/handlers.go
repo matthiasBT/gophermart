e3a0cbee834357f732e3f23807b1168a90772177
Temur Uzbekov <barkerhenry840@gmail.com>
2023-10-13 21:32:42 +0300

    Add session token after user creation

--- internal/server/usecases/handlers.go
+++ internal/server/usecases/handlers.go
@@ -1,8 +1,11 @@
 package usecases
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/matthiasBT/gophermart/internal/server/entities"
 )
@@ -12,7 +15,7 @@ func (c *BaseController) register(w http.ResponseWriter, r *http.Request) {
 	if userReq == nil {
 		return
 	}
-	user, err := c.Stor.CreateUser(r.Context(), userReq)
+	_, err := c.Stor.CreateUser(r.Context(), userReq)
 	if err != nil {
 		if errors.Is(err, entities.ErrLoginAlreadyTaken) {
 			w.WriteHeader(http.StatusConflict)
@@ -23,6 +26,26 @@ func (c *BaseController) register(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	// todo: set cookie
-	c.Logger.Infof("User: %v", user)
+	authorize(w)
+	return
+}
+
+func generateSessionToken() string {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func authorize(w http.ResponseWriter) {
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_token",
+		Value:    generateSessionToken(),
+		Path:     "/",
+		Expires:  time.Now().Add(120 * time.Second),
+		HttpOnly: true,  // Protect against XSS attacks
+		Secure:   false, // Should be true in production to send only over HTTPS
+	})
 }
